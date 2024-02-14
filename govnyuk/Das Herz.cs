bbed51c42505b619c154bf1fbffcf5c6f2b1468d
julia <myhimmell@gmail.com>
2024-02-14 15:12:02 +0300

    Herrz

---
+++ govnyuk/Das Herz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace ConsoleApplication2
+{
+    class Program
+    {
+       static void Main(string[] args)
+       {
+           float y,x, z, f;
+           for (y =1.5f; y > -1.5f; y -= 0.1f)
+           {
+              for (x = -1.5f; x < 1.5f;x += 0.05f)
+              {
+                 z = x * x + y * y - 1;
+                 f = z * z * z - x * x * y * y * y;
+                 Console.Write(f <= 0.0f ? ".:-=+*#%@"[(int)(f* -8.0f)] : ' ');
+              }
+              Console.WriteLine();
+           }
+          Console.ReadLine();
+ 
+       }
+    }
+}
